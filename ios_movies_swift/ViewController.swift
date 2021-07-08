3657cb5357272a8d94cf86defdaccb185b4bc5c0
Asif <asif.kazmi@sberbank.at>
2021-07-08 18:03:58 +0200

    Initial Commit

---
+++ ios_movies_swift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ios_movies_swift
+//
+//  Created by SanaAsif on 08/07/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
